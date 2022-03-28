57a258393b86612a3fb3bcbbfa71343d415cfa6d
Jose Lopez <jose.m.lopez747@gmail.com>
2022-03-28 15:54:11 -0500

    Exercise 1a complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -7,11 +7,17 @@ public class ControlFlowExercises {
 //
 //While
 //   Create an integer variable i with a value of 5.
+        int i = 5;
 //   Create a while loop that runs so long as i is less than or equal to 15
+        while(i <= 15) {
+            System.out.println(i);
+            i++;
+        }
 //   Each loop iteration, output the current value of i, then increment i by one.
-//
+//i++ does the increment
 //   Your output should look like this:
 //
 //    5 6 7 8 9 10 11 12 13 14 15
+
     }
 }
