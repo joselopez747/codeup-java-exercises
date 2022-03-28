635b8b4717d744dfa57d555380af91ca015b673a
Jose Lopez <jose.m.lopez747@gmail.com>
2022-03-28 12:14:26 -0500

    java I intro

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,84 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        //1
+//        int myFavoriteNumber = 3;
+//        System.out.println("myFavoriteNumber: " + myFavoriteNumber);
+
+
+        //2
+//        String myString = "My first string using Java";
+//        System.out.println("myString: " + myString);
+
+
+        //3
+//        char myString = 'c';
+//        System.out.println(myString);
+
+
+        //4
+//        double myString = 3.14159;
+//        System.out.println("myString: " + myString);
+
+
+        //5
+//        long myNumber = ;
+//        System.out.println(myNumber);
+//         does not work
+
+
+        //6
+//        double myNumber = 3.14;
+//        System.out.println("myNumber: " + myNumber);
+
+
+        //7
+
+
+
+
+        //8
+//        myNumber = 123;
+//        System.out.println(myNumber);
+
+
+        //9
+//        float myNumber = 3.14;
+//        System.out.println(myNumber);
+
+
+        //10
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+
+        //11
+//        class myClass = '';
+//        System.out.println(myClass);
+
+
+        //12
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//
+//        int three = (int) "three";
+
+
+        //13
+        int x = 4;
+        x = x + 5;
+
+        int a = x++ 5;
+
+
+
+
     }
 }
