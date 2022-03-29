05df58cd1be5414017de2ab126d9b59cc0ae3098
Jose Lopez <jose.m.lopez747@gmail.com>
2022-03-29 10:37:49 -0500

    Exercise 2 Fizzbuzz started

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -67,6 +67,17 @@ public class ControlFlowExercises {
 //        }
 
 
+// Exercise 2 Fizzbuzz
+// One of the most common interview questions for entry-level programmers is the FizzBuzz test. Developed by Imran Ghory, the test is designed to assess basic looping and conditional logic skills.
+//
+// Write a program that prints the numbers from 1 to 100.
+// For multiples of three: print “Fizz” instead of the number.
+// For the multiples of five: print “Buzz”.
+// For numbers which are multiples of both three and five: print “FizzBuzz”.
+
+
+
+
 
     }
 }
