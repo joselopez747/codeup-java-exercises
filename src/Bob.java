a7072205f259fc704074d8f67658d0e73973c5a3
Jose Lopez <jose.m.lopez747@gmail.com>
2022-03-30 10:30:36 -0500

    Bob Exercise Complete

--- src/Bob.java
+++ src/Bob.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
+
+// Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+//
+// Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
+// He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
+// He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
+// He answers 'Whatever.' to anything else.
+//
+// Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please say something to Bob...");
+        String userInput = sc.nextLine();
+        if (userInput.endsWith("?")){
+            System.out.println("Sure");
+        } else if (userInput.endsWith("!")){
+            System.out.println("Whoa, chill out!");
+        } else if (userInput.isEmpty()){
+            System.out.println("Fine. Be that way!");
+        } else {
+            System.out.println("Whatever");
+        }
+
+
+
+
 
     }
 }
