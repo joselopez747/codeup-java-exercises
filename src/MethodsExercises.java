9ec923406eda0652e914908b80e714c339140982
Jose Lopez <jose.m.lopez747@gmail.com>
2022-03-30 15:21:56 -0500

    MethodExercise 3 Started

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -45,22 +45,44 @@ public class MethodsExercises {
     // Methods Exercise 2
     // create a method that validates that user input is in a certain range.
 
-    public static int getInteger(int min, int max){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number between " + min + " and " + max);
-        int userInput = sc.nextInt();
+//    public static int getInteger(int min, int max){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number between " + min + " and " + max);
+//        int userInput = sc.nextInt();
+//
+//        //this starts the loop
+//        if (userInput < min || userInput > max) {
+//            System.out.println("Number is out of range");
+//            return getInteger(min, max);
+//        } else {
+//
+//        //this stops the loop
+//            System.out.println("Number is in range");
+//            return userInput;
+//        }
+//    }
 
-        //this starts the loop
-        if (userInput < min || userInput > max) {
-            System.out.println("Number is out of range");
-            return getInteger(min, max);
-        } else {
 
-        //this stops the loop
-            System.out.println("Number is in range");
-            return userInput;
-        }
-    }
+
+
+    //Method Exercise 3
+//    Calculate the factorial of a number.
+//
+//    Prompt the user to enter an integer from 1 to 10.
+//    Display the factorial of the number entered by the user.
+//    Ask if the user wants to continue.
+//    Use a for loop to calculate the factorial.
+//    Assume that the user will enter an integer, but verify it’s between 1 and 10.
+//    Use the long type to store the factorial.
+//    Continue only if the user agrees to.
+//    A factorial is a number multiplied by each of the numbers before it.
+//
+//    Factorials are denoted by the exclamation point (n!). Ex:
+// 1! = 1               = 1
+// 2! = 1 x 2           = 2
+// 3! = 1 x 2 x 3       = 6
+// 4! = 1 x 2 x 3 x 4   = 24
+
 
 
 
