7cfe2a11fb53212056c6ec4d4650b593277b8939
Jose Lopez <jose.m.lopez747@gmail.com>
2022-04-01 14:39:12 -0500

    continued mini excercise part 2

--- src/DishTest.java
+++ src/DishTest.java
@@ -1,42 +1,109 @@
 class Dish {
-    int costInCents;
-    String nameOfDish;
-    boolean wouldRecommend;
-    static int totalDishes = 0;
+    private int costInCents;
+    private String nameOfDish;
+    private boolean wouldRecommend;
 
-    public Dish(){
+    //Default # of dishes
+    private static int totalDishes = 0;
+
+    public Dish() {
+        //so much logic!!!!!
         totalDishes++;
     }
 
-    public void printSummary(){
+    //this is the constructor
+    public Dish(int costInCents, String nameOfDish, boolean wouldRecommend) {
+        this();
+        this.costInCents = costInCents;
+        this.nameOfDish = nameOfDish;
+        this.wouldRecommend = wouldRecommend;
+    }
 
+    public int getCostInCents() {
+        return costInCents;
+    }
 
-        System.out.printf(
-                "\nCost: %d \n Name: %s \n Recommended: %b \n", costInCents, nameOfDish, wouldRecommend);
+    public String getNameOfDish() {
+        return nameOfDish;
+    }
+
+    public boolean isWouldRecommend() {
+        return wouldRecommend;
+    }
+
+    public static int getTotalDishes() {
+        return totalDishes;
+    }
+
+    public void setCostInCents(int costInCents) {
+        this.costInCents = costInCents;
+    }
+
+    public void setNameOfDish(String nameOfDish) {
+        this.nameOfDish = nameOfDish;
+    }
+
+    public void isWouldRecommend(boolean wouldRecommend) {
+        this.wouldRecommend = wouldRecommend;
+    }
+
+    public static void setTotalDishes(int totalDishes) {
+        Dish.totalDishes = totalDishes;
     }
 
 
+//
+//    // getters and setters
+//    public int getCostInCents(){
+//        return costInCents;
+//    }
+//    public void setCostInCents(int costInCents){
+//        this.costInCents = costInCents;
+//    }
+//
+//
+//    public String getNameOfDish(){
+//        return nameOfDish;
+//    }
+//    public void setNameOfDish(String nameOfDish){
+//        this.nameOfDish = nameOfDish;
+//    }
+//
+//
+//    public boolean isWouldRecommend(){
+//        return wouldRecommend;
+//    }
+//    public void setWouldRecommend(boolean wouldRecommend){
+//        this.wouldRecommend = wouldRecommend;
+//    }
+//
+//    public Dish (int totalDishes){
+//        return totalDishes;
+//    }
+//    public void Dish (int totalDishes){
+//        this.totalDishes = totalDishes;
+//    }
+//
+//
 
+
+
+
+    public void printSummary(){
+        System.out.printf(
+                "\nCost: %d \n Name: %s \n Recommended: %b \n", costInCents, nameOfDish, wouldRecommend);
+
+    }
 }
-
-
+//
+//
 public class DishTest {
     public static void main(String[] args) {
 
-        Dish dish1 = new Dish();
-
-        dish1.nameOfDish = "Dragon Roll";
-        dish1.costInCents = 15000;
-        dish1.wouldRecommend = true;
-
-        dish1.printSummary();
-
-        Dish dish2 = new Dish();
-
-        dish2.printSummary();
-
-        System.out.println("\nTotal Dishes: " + Dish.totalDishes);
+        Dish dish1 = new Dish(1500, "Dragon Roll", true);
+        Dish dish2 = new Dish(2500, "Rainbow Roll", true);
+        Dish dish3 = new Dish(3500, "Sushi roll", false);
+        System.out.println(Dish.getTotalDishes());
 
     }
-
 }
