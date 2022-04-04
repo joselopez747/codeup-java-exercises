99efcff2d70160fba7402888ac8f153aca709f0a
Jose Lopez <jose.m.lopez747@gmail.com>
2022-04-04 09:58:25 -0500

    Monster Test Class Created

---
+++ src/MonsterTest.java
@@ -0,0 +1,9 @@
+public class MonsterTest {
+
+    Monster orc = new Monster("Orc", 13, 15, 9);
+    Monster blueDragon = new Monster("Blue Dragon", 19, 225, 23);
+    Monster ogre = new Monster("Ogre", 11, 59, 13);
+
+    public static void main(String[] args) {
+    }
+}
