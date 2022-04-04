98f25bbc6062cfd286272c865dc75bf48e93299d
Jose Lopez <jose.m.lopez747@gmail.com>
2022-04-04 11:08:00 -0500

    Arrays Exercises Class Created

---
+++ src/ArraysExercises.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class ArraysExercises {
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(numbers);//this does not print out the array
+//        System.out.println(Arrays.toString(numbers));//this does print out the array but have to have the import class set
+
+
+
+    }
+}
