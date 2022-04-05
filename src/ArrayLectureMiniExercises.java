9a8f46b1dd9044d65b4c6f51a4db61ea3ef356ef
Jose Lopez <jose.m.lopez747@gmail.com>
2022-04-05 13:14:21 -0500

    Array Mini Exercise

--- src/ArrayLectureMiniExercises.java
+++ src/ArrayLectureMiniExercises.java
@@ -1,9 +1,12 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class ArrayLectureMiniExercises {
 
-
+public static String[] person = {"John", "Paul", "Ringo"};
 
 
     public static void main(String[] args) {
+        System.out.println(person.length);
 
     }
 }
